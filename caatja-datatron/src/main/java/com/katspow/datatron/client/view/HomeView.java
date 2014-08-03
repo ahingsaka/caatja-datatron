3b240a5fe3973ba0b5fedc96b2758f1ef593121d
AHINGSAKA SAMSENESENA <samizaka@gmail.com>
2014-08-03 22:10:38 +0200

    info msg ui binded

--- caatja-datatron/src/main/java/com/katspow/datatron/client/view/HomeView.java
+++ caatja-datatron/src/main/java/com/katspow/datatron/client/view/HomeView.java
@@ -5,9 +5,10 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class HomeView extends Composite {
@@ -15,20 +16,37 @@ public class HomeView extends Composite {
     interface MyUiBinder extends UiBinder<Widget, HomeView> {
     }
 
+    private static final String WELCOME = "Welcome to the DATATRON ! Please select an action on the left menu.";
+
     private static MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
+
+    @UiField
+    Anchor listApp;
 
     @UiField
     Anchor newApp;
 
+    @UiField
+    HTMLPanel mainPanel;
+
+    @UiField
+    HTML infoMsg;
+
     public HomeView() {
         initWidget(uiBinder.createAndBindUi(this));
-        
-        newApp.addClickHandler(new ClickHandler() {
+
+        setInfoMsg(WELCOME);
+
+        listApp.addClickHandler(new ClickHandler() {
             @Override
             public void onClick(ClickEvent event) {
-                Window.alert("new app");
+                mainPanel.clear();
             }
         });
+    }
+
+    public void setInfoMsg(String msg) {
+        infoMsg.setHTML("<h4 class='alert_info'>" + msg + "</h4>");
     }
 
 }
