04aa4526467e6f2cd52e3383c9f7e1944b593b5f
AHINGSAKA SAMSENESENA <samizaka@gmail.com>
2014-08-03 15:32:40 +0200

    initial commit

---
+++ caatja-datatron/src/main/java/com/katspow/datatron/client/Datatron.java
@@ -0,0 +1,13 @@
+package com.katspow.datatron.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Datatron implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
