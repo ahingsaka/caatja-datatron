9bc00b2378c657dc04cfab913c27716e455b041b
AHINGSAKA SAMSENESENA <samizaka@gmail.com>
2014-08-06 21:55:17 +0200

    prepare add application name popup

--- caatja-datatron/src/main/java/com/katspow/datatron/client/view/popup/CreateAppPopup.java
+++ caatja-datatron/src/main/java/com/katspow/datatron/client/view/popup/CreateAppPopup.java
@@ -1,14 +1,51 @@
 package com.katspow.datatron.client.view.popup;
 
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.Widget;
 
-public class CreateAppPopup extends PopupPanel {
+public class CreateAppPopup extends Composite {
+    
+    interface CreateAppPopupUiBinder extends UiBinder<Widget, CreateAppPopup> {
+    }
+    
+    private static CreateAppPopupUiBinder uiBinder = GWT.create(CreateAppPopupUiBinder.class);
+    
+    @UiField
+    HTMLPanel container;
+    
+    @UiField
+    DialogBox dialogBox;
     
     public CreateAppPopup() {
-        super(true);
-        setGlassEnabled(true);
-        setWidget(new Label("Click outside to close"));
+        
+        initWidget(uiBinder.createAndBindUi(this));
+        
+        dialogBox.setGlassEnabled(true);
+        dialogBox.setModal(true);
+        dialogBox.setAnimationEnabled(true);
+        
+//        setWidget(new Label("Click outside to close"));
+//        
+//        errorLabel = new Label();
+//        
+//        final Button submitBtn = new Button("Submit");
+//        
+//        nameField = new TextBox();
+//        
+//        submitBtn.addClickHandler(new ClickHandler() {
+//            public void onClick(ClickEvent event) {
+////                callService();
+//            }
+//        });
+    }
+
+    public void center() {
+        dialogBox.center();
     }
 
 }
