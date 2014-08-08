990b2ebb4bfe00936c83f56eb5b618bcbe641236
AHINGSAKA SAMSENESENA <samizaka@gmail.com>
2014-08-08 23:42:48 +0200

    add objectify dependency

---
+++ caatja-datatron/src/main/java/com/katspow/datatron/server/entity/DatatronApplication.java
@@ -0,0 +1,38 @@
+package com.katspow.datatron.server.entity;
+
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+import com.googlecode.objectify.annotation.Parent;
+
+public class DatatronApplication {
+    
+    @Parent Key parent;
+
+    @Id Long id;
+    
+    @Index
+    private String name;
+    
+    public DatatronApplication() {
+    }
+
+    public DatatronApplication(String name) {
+        this.name = name;
+        this.parent = KeyFactory.createKey("RootApp", "app");
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public Key getParent() {
+        return parent;
+    }
+
+}
