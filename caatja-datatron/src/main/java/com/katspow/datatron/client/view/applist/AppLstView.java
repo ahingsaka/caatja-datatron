6ee852319a70040ece15124ed5672d31629bbbb8
AHINGSAKA SAMSENESENA <samizaka@gmail.com>
2014-08-09 23:26:05 +0200

    prepare delete action

--- caatja-datatron/src/main/java/com/katspow/datatron/client/view/applist/AppLstView.java
+++ caatja-datatron/src/main/java/com/katspow/datatron/client/view/applist/AppLstView.java
@@ -27,31 +27,30 @@ import com.katspow.datatron.client.view.popup.DatatronPopup;
 import com.katspow.datatron.shared.ApplicationDto;
 
 public class AppLstView extends Composite {
-    
+
     interface MyUiBinder extends UiBinder<Widget, AppLstView> {
     }
-    
+
     private static MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
-    
+
     private static final DatatronServiceAsync dataService = GWT.create(DatatronService.class);
-    
+
     @UiField(provided = true)
     CellTable<ApplicationDto> appLst;
-    
+
     public AppLstView() {
         appLst = new CellTable<ApplicationDto>(10, (Resources) GWT.create(TableResources.class));
         initWidget(uiBinder.createAndBindUi(this));
-        
-        
+
         TextColumn<ApplicationDto> nameColumn = new TextColumn<ApplicationDto>() {
             public String getValue(ApplicationDto object) {
                 return object.name;
             }
-            
+
         };
-        
+
         appLst.addColumn(nameColumn, "Name");
-        
+
         final SafeHtmlCell selectCell = new SafeHtmlCell() {
             @Override
             public Set<String> getConsumedEvents() {
@@ -61,8 +60,7 @@ public class AppLstView extends Composite {
             }
         };
 
-        Column<ApplicationDto, SafeHtml> selectCol = new Column<ApplicationDto, SafeHtml>(
-                selectCell) {
+        Column<ApplicationDto, SafeHtml> selectCol = new Column<ApplicationDto, SafeHtml>(selectCell) {
             public SafeHtml getValue(ApplicationDto value) {
                 SafeHtmlBuilder sb = new SafeHtmlBuilder();
                 sb.appendHtmlConstant("<input type='image' src='images/icn_search.png' title='Search' />");
@@ -71,41 +69,58 @@ public class AppLstView extends Composite {
 
             @Override
             public void onBrowserEvent(Context context, Element elem, ApplicationDto object, NativeEvent event) {
-                super.onBrowserEvent(context, elem, object, event); 
+                super.onBrowserEvent(context, elem, object, event);
                 if ("click".equals(event.getType())) {
-                    DatatronPopup datatronPopup = new DatatronPopup("Confirm", "This app will be selected");
+                    DatatronPopup datatronPopup = new DatatronPopup("Confirm action", "Application <b>"
+                            + object.getName() + "</b> will be selected");
                     datatronPopup.center();
                 }
             }
-            
-        };
-        
-        
-        appLst.addColumn(selectCol, "Select");
-        
-        final SafeHtmlCell progressCell = new SafeHtmlCell();
 
-        Column<ApplicationDto, SafeHtml> progressCol = new Column<ApplicationDto, SafeHtml>(
-                progressCell) {
+        };
+
+        appLst.addColumn(selectCol, "Select");
+
+        final SafeHtmlCell progressCell = new SafeHtmlCell() {
+            @Override
+            public Set<String> getConsumedEvents() {
+                HashSet<String> events = new HashSet<String>();
+                events.add("click");
+                return events;
+            }
+        };
+
+        Column<ApplicationDto, SafeHtml> progressCol = new Column<ApplicationDto, SafeHtml>(progressCell) {
             public SafeHtml getValue(ApplicationDto value) {
                 SafeHtmlBuilder sb = new SafeHtmlBuilder();
                 sb.appendHtmlConstant("<input type='image' src='images/icn_trash.png' title='Trash' />");
                 return sb.toSafeHtml();
             }
+
+            @Override
+            public void onBrowserEvent(Context context, Element elem, ApplicationDto object, NativeEvent event) {
+                super.onBrowserEvent(context, elem, object, event);
+                if ("click".equals(event.getType())) {
+                    DatatronPopup datatronPopup = new DatatronPopup("Confirm action", "Application <b>"
+                            + object.getName() + "</b> and all associated resources will be <b>DELETED</b> !");
+                    datatronPopup.center();
+                }
+            }
+
         };
-        
+
         appLst.addColumn(progressCol, "Delete");
-        
+
         dataService.findAllApps(new AsyncCallback<List<ApplicationDto>>() {
             public void onSuccess(List<ApplicationDto> result) {
                 appLst.setRowData(result);
             }
-            
+
             @Override
             public void onFailure(Throwable caught) {
             }
         });
-        
+
     }
 
 }
