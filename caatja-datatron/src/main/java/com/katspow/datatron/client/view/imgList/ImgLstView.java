395176b25eb9fd1db7d22cafd1497dd529484fcd
AHINGSAKA SAMSENESENA <samizaka@gmail.com>
2014-08-11 23:17:18 +0200

    prepare image cell

--- caatja-datatron/src/main/java/com/katspow/datatron/client/view/imgList/ImgLstView.java
+++ caatja-datatron/src/main/java/com/katspow/datatron/client/view/imgList/ImgLstView.java
@@ -1,9 +1,14 @@
 package com.katspow.datatron.client.view.imgList;
 
+import com.google.gwt.cell.client.AbstractCell;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.cellview.client.CellList;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
+import com.katspow.datatron.shared.ImageDto;
 
 public class ImgLstView extends Composite {
 
@@ -12,14 +17,37 @@ public class ImgLstView extends Composite {
     interface ImgLstViewUiBinder extends UiBinder<Widget, ImgLstView> {
     }
 
-    public ImgLstView() {
-        initWidget(uiBinder.createAndBindUi(this));
-    }
-    
-    public ImgLstView(String firstName) {
-        initWidget(uiBinder.createAndBindUi(this));
+    @UiField(provided = true)
+    CellList<ImageDto> imgLst;
+
+    static class ImageCell extends AbstractCell<ImageDto> {
+
+        @Override
+        public void render(com.google.gwt.cell.client.Cell.Context context, ImageDto value, SafeHtmlBuilder sb) {
+            if (value == null) {
+                return;
+            }
+
+            sb.appendHtmlConstant("<table>");
+
+            // Add the contact image.
+            sb.appendHtmlConstant("<tr><td rowspan='3'>");
+            // sb.appendHtmlConstant(imageHtml);
+            sb.appendHtmlConstant("</td>");
+
+            // Add the name and address.
+            sb.appendHtmlConstant("<td style='font-size:95%;'>");
+            sb.appendEscaped("Test");
+            // sb.appendHtmlConstant("</td></tr><tr><td>");
+            // sb.appendEscaped(value.getAddress());
+            // sb.appendHtmlConstant("</td></tr></table>");
+        }
     }
 
-   
+    public ImgLstView() {
+        ImageCell imageCell = new ImageCell();
+        imgLst = new CellList<ImageDto>(imageCell);
+        initWidget(uiBinder.createAndBindUi(this));
+    }
 
 }
---
+++ caatja-datatron/src/main/java/com/katspow/datatron/shared/ImageDto.java
@@ -0,0 +1,12 @@
+package com.katspow.datatron.shared;
+
+import java.io.Serializable;
+
+public class ImageDto implements Serializable {
+
+    private static final long serialVersionUID = -4001369304426534002L;
+    
+    public ImageDto() {
+    }
+
+}
