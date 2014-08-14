f7d35f74089aaedcb112e7608c6aeed6967007ea
AHINGSAKA SAMSENESENA <samizaka@gmail.com>
2014-08-14 23:13:33 +0200

    work on gwtupload integration

--- caatja-datatron/src/main/java/com/katspow/datatron/client/view/upload/UploadView.java
+++ caatja-datatron/src/main/java/com/katspow/datatron/client/view/upload/UploadView.java
@@ -1,5 +1,11 @@
 package com.katspow.datatron.client.view.upload;
 
+import gwtupload.client.IUploadStatus.Status;
+import gwtupload.client.IUploader;
+import gwtupload.client.IUploader.OnFinishUploaderHandler;
+import gwtupload.client.IUploader.UploadedInfo;
+import gwtupload.client.MultiUploader;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
@@ -29,8 +35,18 @@ public class UploadView extends Composite {
 
     @UiField
     SubmitButton cancelBtn;
+    
+    @UiField(provided = true)
+    MultiUploader uploader;
 
     public UploadView() {
+        
+        uploader = new MultiUploader();
+        uploader.setAutoSubmit(false);
+        uploader.setValidExtensions(".jpg", ".png", ".gif");
+        uploader.addOnFinishUploadHandler(onFinishUploaderHandler);
+        uploader.reset();
+        
         initWidget(uiBinder.createAndBindUi(this));
 
         dialogBox.setGlassEnabled(true);
@@ -47,7 +63,45 @@ public class UploadView extends Composite {
             public void onClick(ClickEvent event) {
             }
         });
+        
     }
+    
+    private OnFinishUploaderHandler onFinishUploaderHandler = new OnFinishUploaderHandler() {
+        @Override
+        public void onFinish(IUploader uploader) {
+            
+            if (uploader.getStatus() == Status.SUCCESS) {
+
+//                new PreloadedImage(uploader.fileUrl(), showImage);
+                
+                // The server sends useful information to the client by default
+                UploadedInfo info = uploader.getServerInfo();
+                System.out.println("File name " + info.name);
+                System.out.println("File content-type " + info.ctype);
+                System.out.println("File size " + info.size);
+                
+//                Long appId = Long.valueOf(applstBox.getValue(applstBox.getSelectedIndex()));
+//
+//                gruiService.findAllResources( appId, new AsyncCallback<List<ResourceDto>>() {
+//                    @Override
+//                    public void onSuccess(List<ResourceDto> result) {
+//                        resources = result;
+//                        imgListBox.clear();
+//                        for (ResourceDto resourceDto : result) {
+//                            imgListBox.addItem(resourceDto.getName(), String.valueOf(resourceDto.getId()));
+//                        }
+//                    }
+//                    
+//                    @Override
+//                    public void onFailure(Throwable caught) {
+//                        // TODO Auto-generated method stub
+//                    }
+//                });
+
+             }
+            
+        }
+    };
 
     public void center() {
         dialogBox.center();
