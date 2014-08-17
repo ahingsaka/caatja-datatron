d255462d1003ccbaee6b7f2a95043b583c772fa0
AHINGSAKA SAMSENESENA <samizaka@gmail.com>
2014-08-17 11:37:01 +0200

    clean img popup

--- caatja-datatron/src/main/java/com/katspow/datatron/client/view/imgList/ImgPopup.java
+++ caatja-datatron/src/main/java/com/katspow/datatron/client/view/imgList/ImgPopup.java
@@ -7,9 +7,7 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Image;
-import com.google.gwt.user.client.ui.SubmitButton;
 import com.google.gwt.user.client.ui.Widget;
 import com.katspow.datatron.shared.ImageDto;
 
@@ -24,14 +22,8 @@ public class ImgPopup extends Composite {
     DialogBox dialogBox;
 
     @UiField
-    HTML title;
-    
-    @UiField
     Image image;
     
-    @UiField
-    SubmitButton okBtn;
-
     public ImgPopup(ImageDto imageDto) {
         initWidget(uiBinder.createAndBindUi(this));
         
@@ -39,17 +31,14 @@ public class ImgPopup extends Composite {
         dialogBox.setModal(true);
         dialogBox.setAnimationEnabled(true);
         
-        okBtn.addClickHandler(new ClickHandler() {
+        String base64Data = "data:image/png;base64," + imageDto.getImageData();
+        image.setVisibleRect(0, 0, image.getWidth(), image.getHeight());
+        image.setUrl(base64Data);
+        image.addClickHandler(new ClickHandler() {
             public void onClick(ClickEvent event) {
                 dialogBox.hide();
             }
         });
-        
-        title.setHTML("<h3>" + imageDto.getName() + "</h3>");
-        
-        String base64Data = "data:image/png;base64," + imageDto.getImageData();
-        image.setVisibleRect(0, 0, image.getWidth(), image.getHeight());
-        image.setUrl(base64Data);
     }
 
     public void center() {
