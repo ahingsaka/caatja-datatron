36bd09f5aa34eba9bcec1dcf3c294b95fb009963
AHINGSAKA SAMSENESENA <samizaka@gmail.com>
2014-08-23 17:01:11 +0200

    work on password view

--- caatja-datatron/src/main/java/com/katspow/datatron/client/view/pwd/PasswordView.java
+++ caatja-datatron/src/main/java/com/katspow/datatron/client/view/pwd/PasswordView.java
@@ -1,9 +1,15 @@
 package com.katspow.datatron.client.view.pwd;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.SubmitButton;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
+import com.katspow.datatron.client.Datatron;
 
 public class PasswordView extends Composite {
 
@@ -11,9 +17,28 @@ public class PasswordView extends Composite {
 
     interface PasswordViewUiBinder extends UiBinder<Widget, PasswordView> {
     }
+    
+    @UiField
+    TextBox currentPwd;
+    
+    @UiField
+    TextBox newPwd;
+    
+    @UiField
+    TextBox confirmPwd;
+    
+    @UiField
+    SubmitButton saveBtn;
 
     public PasswordView() {
         initWidget(uiBinder.createAndBindUi(this));
+        currentPwd.setEnabled(false);
+        
+        saveBtn.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                Datatron.showHomeView();
+            }
+        });
     }
 
 }
