a34ce1938799448d453e43b216e99285e8c5b69d
AHINGSAKA SAMSENESENA <samizaka@gmail.com>
2014-08-31 15:37:02 +0200

    prepare rest resource

--- caatja-datatron/src/main/java/com/katspow/datatron/server/rest/application/DatatronRestApplication.java
+++ caatja-datatron/src/main/java/com/katspow/datatron/server/rest/application/DatatronRestApplication.java
@@ -1,7 +1,79 @@
 package com.katspow.datatron.server.rest.application;
 
-import org.restlet.Application;
+import java.util.List;
 
+import org.restlet.Application;
+import org.restlet.Request;
+import org.restlet.Response;
+import org.restlet.Restlet;
+import org.restlet.data.ChallengeScheme;
+import org.restlet.routing.Router;
+import org.restlet.security.ChallengeAuthenticator;
+import org.restlet.security.MapVerifier;
+
+import com.google.appengine.api.datastore.KeyFactory;
+import com.googlecode.objectify.ObjectifyService;
+import com.katspow.datatron.server.entity.DatatronApplication;
+import com.katspow.datatron.server.rest.resource.DatatronImageResource;
+
+/**
+ * Cross site request
+ * http://restlet-discuss.1400322.n2.nabble.com/Cross-Domain-JQUERY
+ * -POST-Attempt-td6222104.html
+ * 
+ * @author Ahingsaka
+ *
+ */
 public class DatatronRestApplication extends Application {
+
+    static {
+        ObjectifyService.register(DatatronApplication.class);
+    }
+
+    @Override
+    public Restlet createInboundRoot() {
+        Router router = new Router(getContext());
+        router.attachDefault(DatatronImageResource.class);
+
+        ChallengeAuthenticator challengeAuthenticator = new ChallengeAuthenticator(getContext(),
+                ChallengeScheme.HTTP_BASIC, "myRealm") {
+            @Override
+            protected boolean authenticate(Request request, Response response) {
+                if (request.getChallengeResponse() == null) {
+                    return false;
+                } else {
+                    return super.authenticate(request, response);
+                }
+            }
+        };
+
+        OriginFilter originFilter = new OriginFilter(getContext());
+        // originFilter.setNext(challengeAuthenticator);
+
+        List<DatatronApplication> listFound = ObjectifyService.ofy().load().type(DatatronApplication.class)
+                .ancestor(KeyFactory.createKey("RootApp", "app")).list();
+
+        if (!listFound.isEmpty()) {
+
+            MapVerifier verifier = new MapVerifier();
+
+            for (DatatronApplication datatronApplication : listFound) {
+                String appName = datatronApplication.getName();
+                String password = datatronApplication.getPassword();
+
+                verifier.getLocalSecrets().put(appName, password.toCharArray());
+                challengeAuthenticator.setVerifier(verifier);
+            }
+
+            // challengeAuthenticator.setNext(router);
+
+            originFilter.setNext(challengeAuthenticator);
+            challengeAuthenticator.setNext(router);
+
+        }
+
+        return originFilter;
+        // return challengeAuthenticator;
+    }
 
 }
--- caatja-datatron/src/main/java/com/katspow/datatron/server/rest/resource/DatatronImageResource.java
+++ caatja-datatron/src/main/java/com/katspow/datatron/server/rest/resource/DatatronImageResource.java
@@ -1,7 +1,39 @@
 package com.katspow.datatron.server.rest.resource;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.restlet.representation.Representation;
+import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
+import org.restlet.ext.json.JsonRepresentation;
+
+import com.googlecode.objectify.Objectify;
+import com.googlecode.objectify.ObjectifyService;
 
 public class DatatronImageResource extends ServerResource {
+
+    @Get
+    public Representation represent() {
+
+        String appName = getQuery().getValues("appName");
+
+        Map<String, String> valuesMap = getQuery().getValuesMap();
+        Map<String, String> responseMap = new HashMap<String, String>();
+        JSONArray jsonData = new JSONArray();
+
+        Objectify ofy = ObjectifyService.ofy();
+
+        // String pictureName = getQuery().getValues("pictureName");
+        // GruiResource resource =
+        // ofy.load().type(GruiResource.class).ancestor(app).filter("name",
+        // pictureName).first().now();
+        // return resource.getImageData();
+
+        jsonData.put(responseMap);
+
+        return new JsonRepresentation(jsonData);
+    }
 
 }
