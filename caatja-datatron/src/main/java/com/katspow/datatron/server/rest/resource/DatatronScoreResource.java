f2c03bdbdb0751dcf38bb9de26d16178a114b571
AHINGSAKA SAMSENESENA <samizaka@gmail.com>
2014-09-18 22:52:28 +0200

    create score resource

---
+++ caatja-datatron/src/main/java/com/katspow/datatron/server/rest/resource/DatatronScoreResource.java
@@ -0,0 +1,47 @@
+package com.katspow.datatron.server.rest.resource;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.restlet.ext.json.JsonRepresentation;
+import org.restlet.representation.Representation;
+import org.restlet.resource.Get;
+import org.restlet.resource.ServerResource;
+
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.Objectify;
+import com.googlecode.objectify.ObjectifyService;
+import com.katspow.datatron.server.entity.DatatronApplication;
+import com.katspow.datatron.server.entity.DatatronRoot;
+import com.katspow.datatron.server.entity.DatatronScore;
+
+public class DatatronScoreResource extends ServerResource {
+
+    @Get
+    public Representation represent() {
+
+        String appName = getQuery().getValues("appName");
+
+        Map<String, Integer> responseMap = new HashMap<String, Integer>();
+        JSONArray jsonData = new JSONArray();
+
+        Objectify ofy = ObjectifyService.ofy();
+        DatatronApplication app = ofy.load().type(DatatronApplication.class)
+                .ancestor(Key.create(DatatronRoot.class, "app")).filter("name", appName).first().now();
+
+        List<DatatronScore> listFound = ObjectifyService.ofy().load().type(DatatronScore.class).ancestor(app)
+                .order("numOrder").list();
+
+        for (DatatronScore datatronScore : listFound) {
+            responseMap.put(datatronScore.getName(), datatronScore.getScore());
+        }
+
+        jsonData.put(responseMap);
+
+        return new JsonRepresentation(jsonData);
+
+    }
+
+}
